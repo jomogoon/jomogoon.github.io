da6b57875c4fabfacc680c5f29b496019f51ca51
Jo <jomogun@gmail.com>
2016-01-07 00:39:22 +0900

    rebuilding site Thu Jan  7 00:39:22 JST 2016

---
+++ deploy-hugo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploy change to http://jomogoon.github.io ...\033[0m"
+
+# Build
+hugo
+
+# Add changes
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source
+git push origin master
+
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Back to source
+cd ..
+
+git push origin source
+
+
